c70b8d45dfcf90773d3c9f3b1a58321264906bf0
Pablo Menz <p.menz.r@gmail.com>
2020-03-18 19:34:35 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog 
+  
+  def initilize(name)
+    @name = name 
+    @breed = breed 
+  end
+  
+  def name 
+    @name 
+  end
+  
+  def breed 
+    @breed
+  end
+  
+  def name = (new_name)
+    @name = new_name
+  end
+
+  def breed = (dog_breed)
+  @breed = dog_breed
+  end
+  
+end
+
